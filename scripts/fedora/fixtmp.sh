7db855fd51fd1e088ed34fdff70659be02ad830f
Doğukan Çağatay <dcagatay@gmail.com>
2023-03-27 17:27:36 +0200

    Add enabled service check for fix tmp script

--- scripts/fedora/fixtmp.sh
+++ scripts/fedora/fixtmp.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Otherwise the tmp directory is a tiny ramdisk.
-systemctl disable tmp.mount
-systemctl stop tmp.mount
-systemctl mask tmp.mount
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
+# Otherwise the tmp directory is a tiny ramdisk.
+if service_exists tmp.mount; then
+    systemctl disable --now tmp.mount
+    systemctl mask tmp.mount
+fi
